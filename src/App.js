22e7675d5070681aaf84277a56ec97c4a288f12b
ChaneaHamilton <chaneahamilton@gmail.com>
2025-05-06 11:06:56 -0600

    added basic html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-
+import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather app
+        <Weather />
         <footer>
           <p>
             This project was coded by {""}
