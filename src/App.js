312641e532e4a991bb5c9ba8397a8b3053e61e2b
ChaneaHamilton <chaneahamilton@gmail.com>
2025-05-06 10:44:03 -0600

    added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -4,27 +4,29 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <footer>
-        <p>
-          This project was coded by {""}
-          <a
-            href="https://github.com/ChaneaHamilton"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Chanea Hamilton
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/ChaneaHamilton/react-weather-app-w5"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open sourced on Github
-          </a>
-        </p>
-      </footer>
+      <div className="container">
+        Weather app
+        <footer>
+          <p>
+            This project was coded by {""}
+            <a
+              href="https://github.com/ChaneaHamilton"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Chanea Hamilton
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/ChaneaHamilton/react-weather-app-w5"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open sourced on Github
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
