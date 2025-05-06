29ff5418c3f97e39e0c283c9b8f3c62ec0fb21b5
ChaneaHamilton <chaneahamilton@gmail.com>
2025-05-06 10:18:45 -0600

    added Github repo and URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      Weather app
+      <footer>
+        <p>
+          This project was coded by {""}
+          <a
+            href="https://github.com/ChaneaHamilton"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Chanea Hamilton
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/ChaneaHamilton/react-weather-app-w5"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on Github
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
 }
